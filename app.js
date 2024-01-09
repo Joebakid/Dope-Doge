145ca2e8e4bd7d0bb865cce7f6a7571d2791bdb8
joebakid <josephbawo@gmail.com>
2024-01-09 01:32:59 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+let currentSlide = 0;
+
+function showSlide(index) {
+  const slides = document.getElementsByClassName("slide");
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[index].style.display = "block";
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % 3;
+  showSlide(currentSlide);
+}
+
+// Initial setup
+showSlide(currentSlide);
+
+// Change slide every 3 seconds
+setInterval(nextSlide, 3000);
+
+//
+
+gsap.fromTo(
+  ".nav-container",
+  { yPercent: -50, opacity: 0 },
+  { yPercent: 0, opacity: 1, duration: 1, ease: "bounce.out" },
+  ">"
+);
+
+gsap.fromTo(
+  ".showcase",
+  { opacity: 0, scale: 2 },
+  {
+    opacity: 1,
+    stagger: 1,
+    duration: 0.5,
+
+    scale: 1,
+    ease: "bounce.out",
+  },
+  ">"
+);
